6607b992c4c02813d7fdde69231ae26ebf21db8a
@Mikist <mikeochango3@gmail.com>
2024-05-10 23:18:29 +0300

    Upated the home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,19 @@
 
 function Home() {
   return (
-    <div>Discover $ Share</div>
+    <section className=" w-full flex-center flex-col">
+       <h1 className=" head_text text-center">
+          Discover & Share
+          <br className=" max-md:hidden" />
+          <span className=" orange_gradient text-center">
+            AI - Powered Prommpts
+          </span>
+       </h1>
+       <p className="desc text-center">
+        Promptopia is ann open sorce AI Prommpting tool for modern world
+        to discover, create and share creative prompts.
+       </p>
+    </section>
   )
 }
 
