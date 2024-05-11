92028e7c28ea0eb5f499aaaf5a535c444ef21573
@Mikist <mikeochango3@gmail.com>
2024-05-11 20:33:54 +0300

    updated the nav  page

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,8 +7,20 @@ import { useState, useEffect } from 'react'
 import  {getProviders, signIn, signOut, useSession}
  from  'next-auth/react'
 function Nav() {
+  
   return (
-    <div>Nav</div>
+    <nav className='flex-between w-full mb-16 pt-3'>
+      <Link href='/' className=' flex gap-2'>
+        
+        <image  src='/assets/images/logo.svg' className=' object-contain'
+        alt="Logo"
+        width={40}
+        height={30}/>
+        <p className='logo_text'>Promptopia</p>
+      </Link>
+      {/**Mobile */}
+      <div></div>
+    </nav>
   )
 }
 
