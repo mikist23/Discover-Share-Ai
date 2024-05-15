6236c23df9d0246f1ced15be41566fc4357b2ad7
@Mikist <mikeochango3@gmail.com>
2024-05-15 23:04:26 +0300

    Updated navbar component

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,7 +7,11 @@ import { useState, useEffect } from 'react'
 import  {getProviders, signIn, signOut, useSession}
  from  'next-auth/react'
 function Nav() {
+  const signout = ()=>{
+    
+  }
   const [count ,setCount] = useState(0)
+  const isUserLogedIn = true
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className=' flex gap-2'>
@@ -18,8 +22,31 @@ function Nav() {
         height={30}/>
         <p className='logo_text'>Promptopia</p>
       </Link>
-      {/**Mobile */}
-      <div></div>
+      {/**Mobile Navigation */}
+         <div className='sm:flex hidden'>
+            {isUserLogedIn ? (
+              <div className='flex gap-3 md:gap-5'>
+                 <Link href="/create-prompt" className='black_btn'>
+                 Create Post
+                 </Link>
+                 <button onClick={signout} className='outline_btn'>
+                  Sign Out
+                 </button>
+
+                 <Link href="/profile" className=''>
+                  <Image
+                  src="/assets/images/logo.svg"
+                  width={37}
+                  height={37}
+                  />
+                 
+                 </Link>
+              </div>
+            ) : (
+              <>
+              </>
+            )}
+         </div>
     </nav>
   )
 }
